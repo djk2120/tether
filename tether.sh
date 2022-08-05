80b05e880f1c277e1d01bd1e4f1589432a668859
Daniel Kennedy <djk2120@columbia.edu>
2022-08-05 15:34:16 -0600

    comments

--- tether.sh
+++ tether.sh
@@ -2,46 +2,43 @@
 joblist=$1
 template=$2
 
-W=0
-
-:>cases.tmp
-
-
+W=0           #track how many jobs are waiting 
+:>cases.tmp   #where to write the new joblist
 while read -r line;
 do
-    args=(${line//,/ })
+    args=(${line//,/ }) #parse csv line to array 
     jobname=${args[0]}
     thiscase=${args[1]}
     setup=${args[2]}
     status=${args[3]}
 
     if [ $status = "queued" ]; then
-
-	
-	#run setup script if needed
+	#run setup script
 	cd $thiscase
 	if [ $setup != "none" ]; then
 	    $setup
 	fi
 	
-	#submit case, capturing job_id
+	#submit case, capturing jobid
 	./case.submit --resubmit-immediate
 	X=$(./xmlquery JOB_IDS)
 	arrX=(${X//:/ })
-	jobid=${arrX[-1]}
+	jobid=${arrX[-1]} #save the last jobid
 	
 	#update job status to submitted
 	cd -
  	echo $jobname","$thiscase","$setup",submitted">>cases.tmp
 	
     elif [ $status = "waiting" ] && [ $W -eq 0 ];then
-	((W++))
 	#prepare next job for queue
 	qj=$jobname".job"
 	cp $template $qj
 	sed -i 's:jobname:'$jobname':g' $qj
 	sed -i 's:joblist:'$joblist':g' $qj
 	sed -i 's:jobid:'$jobid':g' $qj
+
+	#only queue the first waiting job
+	((W++))
 
 	#update job status to queued
 	echo $jobname","$thiscase","$setup",queued">>cases.tmp
@@ -52,10 +49,10 @@ do
 
 done < $joblist
 
+#update joblist with new statuses
+mv cases.tmp $joblist 
 
-mv cases.tmp $joblist
-
+#queue next job
 if [ $W -gt 0 ];then 
-    #queue next job
     qsub $qj
 fi
